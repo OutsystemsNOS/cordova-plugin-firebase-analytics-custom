58faaf8d5c932437249f62113cb5a2caa1d9fe0d
OutsystemsNOS <45230356+OutsystemsNOS@users.noreply.github.com>
2023-05-25 17:06:15 +0100

    Update FirebaseAnalyticsPlugin.java

--- src/android/FirebaseAnalyticsPlugin.java
+++ src/android/FirebaseAnalyticsPlugin.java
@@ -33,8 +33,6 @@ public class FirebaseAnalyticsPlugin extends ReflectiveCordovaPlugin {
     @CordovaMethod
     private void logEvent(String name, JSONObject params, CallbackContext callbackContext) throws JSONException {
         this.firebaseAnalytics.logEvent(name, parse(params));
-        
-        Log.d(TAG, "Params: " + params.toString());
 
         callbackContext.success();
     }
