80f01f593ab23dd95df0d1f537bd10c0fe6ff159
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-03-20 15:29:31 +0000

    Update FirebaseAnalyticsPlugin.h

--- src/ios/FirebaseAnalyticsPlugin.h
+++ src/ios/FirebaseAnalyticsPlugin.h
@@ -2,6 +2,7 @@
 
 @interface FirebaseAnalyticsPlugin : CDVPlugin
 
+- (void)setConsent:(CDVInvokedUrlCommand*)command;
 - (void)logEvent:(CDVInvokedUrlCommand*)command;
 - (void)setUserId:(CDVInvokedUrlCommand*)command;
 - (void)setUserProperty:(CDVInvokedUrlCommand*)command;
